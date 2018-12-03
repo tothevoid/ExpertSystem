0363633a05e8ec54030ba62c5170062c150dd660
n.filatov <nick9100@yandex.ru>
2018-12-04 01:48:18 +0300

    modifiedTree and emptyFunction

---
+++ core.py
@@ -0,0 +1,8 @@
+from module.nodeGraph import *
+
+if __name__ == "__main__":
+    # t = Tree(Tree('a','b'),Tree('c','d'))
+    # print(t.right.right);
+    # t = TreeModify(TreeModify("a",TreeModify(TreeModify('b',TreeModify('d',TreeModify('e'))),TreeModify('c'))))
+    # print(t.childs.next.childs.val)
+    pass
---
+++ module/nodeGraph.py
@@ -0,0 +1,12 @@
+# class Tree:
+#     def __init__(self,left,right):
+#         self.left = left
+#         self.right = right
+
+class TreeModify:
+    def __init__(self,childs,next=None):
+        self.childs = self.val = childs
+        self.next = next
+
+def fillTree(obj):
+    pass
