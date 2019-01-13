7dc4640e9ede725cb2665c999d5bb64151420d92
s.tokarev <tokarev.sergey337@gmail.com>
2019-01-13 23:29:56 +0300

    core fix

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from ui import UserInterface
+#from ui import UserInterface
 from base import Base
 from db import Db
 
@@ -19,11 +19,11 @@ class ESystem():
         self.user_id = 1
         self.db = Db(Base)
 
-        self.ui = UserInterface(self.user_id, self.db.session)
-        self.ui.init_user_input()
-        self.get_exercises(self.root_id)
+        #self.ui = UserInterface(self.user_id, self.db.session)
+        #self.ui.init_user_input()
+        self.get_excercises(self.root_id)
 
-    def get_exercises(self,child_id):
+    def get_excercises(self,child_id):
         relations = self.db.session.query(Relation).filter_by(parent=child_id)
         if relations.count() == 0:
             return
@@ -34,13 +34,17 @@ class ESystem():
                 if not self.evaulate_rule(rule):
                     isPassed = False
             if isPassed:
-                exercise = self.db.session.query(Exercise).filter_by(node=rel.id)
-                self.excersises.append(exercise)
-                self.get_exercises(rel.child)
+                nodes = self.db.session.query(Node).filter_by(id=rel.parent)
+                for node in nodes:
+                    excercises = self.db.session.query(Exercise).filter_by(id=node.excercise)
+                    for excercise in excercises:
+                        print(excercise)
+                        self.excersises.append(excercise)
+                self.get_excercises(rel.child)
 
     def evaulate_rule(self,rule):
-        rule_info = self.db.session.query(Rule).filter_by(id=rule.id)
-        left_operand = self.db.session.query(UserData).filter_by(user = self.user_id, characteristic = rule_info.left_operand).value
+        rule_info = self.db.session.query(Rule).filter_by(id=rule.id)[0]
+        left_operand = self.db.session.query(UserData).filter_by(user = self.user_id, characteristic = rule_info.left_operand)[0].value
         right_operand = rule_info.right_operand
 
         if rule_info.operator == '<' and int(left_operand) < int(right_operand):
@@ -51,6 +55,5 @@ class ESystem():
             return True
         return False
 
-    
-      
-    
+obj = ESystem()
+print(obj.excersises)
