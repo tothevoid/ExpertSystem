d33bd820ed3fdf88817eeefdeb3a70090ac3f60a
s.tokarev <tokarev.sergey337@gmail.com>
2019-01-15 03:13:15 +0300

    fixget_node_data

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+#TODO: simplify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import mapper
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,9 +7,9 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 class Db:
-    db_log = "root"
-    db_pass = "password"
-    db_url = "mysql://%s:%s@localhost/expert_system" % (db_log, db_pass)
+    db_log = ""
+    db_pass = ""
+    db_url = "mysql://%s:%s@localhost/esystem" % (db_log, db_pass)
     def __init__(self, Base):
         self.Base = Base
         self.engine = create_engine(self.db_url) 
